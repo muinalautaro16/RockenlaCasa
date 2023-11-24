76856616b05c07f1ff2217fec25ea8cf7e986cb2
Lautaro Muiña <87960189+muinalautaro16@users.noreply.github.com>
2023-11-24 15:09:46 -0300

    + Nuevas secciones: .contenido-festival y  .artistas

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
-const { src, dest, watch } = require("gulp");
+const { src, dest, watch, parallel } = require("gulp");
+
+// CSS
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require('gulp-plumber')
+
+
+// Imagenes
+const cache = require("gulp-cache");
+const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
+const avif = require("gulp-avif");
 
 function css(done) {
     src('src/scss/**/*.scss') // Identificar el archivo SASS
@@ -11,6 +20,43 @@ function css(done) {
     done(); // CAllback que avisaa gulp cuando llegamos al final
 }
 
+function imagenes(done) {
+    const opciones = {
+        optimizationLevel: 3
+    }
+
+    src('src/img/**/*.{png,jpg}')
+        .pipe( cache( imagemin(opciones) ) )
+        .pipe( dest('build/img') )
+    done();
+}
+
+function versionWebp(done) {
+
+    const opciones = {
+        quality: 50
+    };
+
+    src('src/img/**/*.{png,jpg}')
+        .pipe( webp(opciones) )
+        .pipe( dest('build/img') )
+
+    done();
+}
+
+function versionAvif(done) {
+
+    const opciones = {
+        quality: 50
+    };
+
+    src('src/img/**/*.{png,jpg}')
+        .pipe( avif(opciones) )
+        .pipe( dest('build/img') )
+
+    done();
+}
+
 function dev(done) {
     watch('src/scss/**/*.scss', css)
 
@@ -18,4 +64,7 @@ function dev(done) {
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.imagenes = imagenes
+exports.webp = versionWebp;
+exports.webp = versionAvif;
+exports.dev = parallel( imagenes, versionWebp, versionAvif, dev );
