0074d1d09d2e8f8441335665decce4f56e08bc34
Lautaro Muiña <87960189+muinalautaro16@users.noreply.github.com>
2023-12-01 03:25:28 -0300

    Finalizacion del proyecto

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,11 @@ const { src, dest, watch, parallel } = require("gulp");
 
 // CSS
 const sass = require("gulp-sass")(require("sass"));
-const plumber = require('gulp-plumber')
-
+const plumber = require("gulp-plumber");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const postcss = require("gulp-postcss");
+const sourcemaps = require("gulp-sourcemaps");
 
 // Imagenes
 const cache = require("gulp-cache");
@@ -11,10 +14,16 @@ const imagemin = require("gulp-imagemin");
 const webp = require("gulp-webp");
 const avif = require("gulp-avif");
 
+// JavaScript
+const terser = require('gulp-terser-js');
+
 function css(done) {
     src('src/scss/**/*.scss') // Identificar el archivo SASS
+        .pipe(sourcemaps.init())
         .pipe( plumber() )
         .pipe( sass() ) // Compilarlo
+        .pipe( postcss([autoprefixer(), cssnano() ]))
+        .pipe(sourcemaps.write('.'))
         .pipe( dest("build/css")); // almacenarla en el disco duro
 
     done(); // CAllback que avisaa gulp cuando llegamos al final
@@ -59,6 +68,7 @@ function versionAvif(done) {
 
 function javascript(done) {
     src('src/js/**/*.js')
+        .pipe( terser() )
         .pipe(dest('build/js'))
 
     done();
